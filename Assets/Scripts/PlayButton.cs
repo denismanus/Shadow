700a24404a46cf5ec145471a38ac87ccda5c907d
okina1raion <sany_z@mail.ua>
2016-10-15 01:08:31 +0300

    Main Menu working canvas

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        Debug.Log("Change Scene");
+        SceneManager.LoadScene("game");
+    }
+    public void ExitGame()
+    {
+        Application.CancelQuit();
+    }
+}
